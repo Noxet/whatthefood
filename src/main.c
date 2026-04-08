4c80393c9b01dda918d5b0527a7ce5e8e955907c
Joanthan Sönnerup <Nooxet@gmail.com>
2026-04-09 00:45:29 +0200

    Added raygui lib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 
 #include "raylib.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 #include "resource_dir.h" // utility header for SearchAndSetResourceDir
 
 #define NUM_RECS 5
@@ -12,6 +15,7 @@ int main()
 
     // Create the window and OpenGL context
     InitWindow(800, 600, "Hello Raylib");
+    GuiLoadStyle("vendor/raygui/styles/terminal/style_terminal.rgs");
 
     // Utility function from resource_dir.h to find the resources folder and set it as the current
     // working directory so we can load from it
@@ -19,13 +23,8 @@ int main()
 
     // Load a texture from the resources directory
     Texture wabbit = LoadTexture("wabbit_alpha.png");
-    Rectangle toggleRecs[NUM_RECS] = {0};
-    int mouseHoverRec = -1;
 
-    for (int i = 0; i < NUM_RECS; i++)
-    {
-        toggleRecs[i] = (Rectangle){40.f, (float)(50 + 32 * i), 150.f, 30.f};
-    }
+    bool showMessageBox = false;
 
     // game loop
     while (!WindowShouldClose()) // run the loop until the user presses ESCAPE or presses the Close
@@ -33,24 +32,22 @@ int main()
     {
         // update logic
 
-        for (int i = 0; i < NUM_RECS; i++)
-        {
-            if (CheckCollisionPointRec(GetMousePosition(), toggleRecs[i]))
-            {
-                mouseHoverRec = i;
-                break;
-            }
-            else
-            {
-                mouseHoverRec = -1;
-            }
-        }
-
         // drawing
         BeginDrawing();
 
         // Setup the back buffer for drawing (clear color and depth buffers)
         ClearBackground(BLACK);
+
+        if (GuiButton((Rectangle){24, 24, 120, 30}, GuiIconText(ICON_RESTART, "Show Message")))
+            showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int res = GuiMessageBox((Rectangle){85, 70, 250, 100}, "#191#Message Box",
+                                    "Hi! This is a message", "Nice;Cool");
+            if (res >= 0)
+                showMessageBox = false;
+        }
 
         // draw some text using the default font
         DrawText("Hello Raylib", 200, 200, 20, WHITE);
@@ -59,11 +56,6 @@ int main()
         DrawTexture(wabbit, 400, 200, WHITE);
 
         DrawFPS(500, 100);
-
-        for (int i = 0; i < NUM_RECS; i++)
-        {
-            DrawRectangleRec(toggleRecs[i], i == mouseHoverRec ? SKYBLUE : LIGHTGRAY);
-        }
 
         // end the frame and get ready for the next one  (display frame, poll input, etc...)
         EndDrawing();
